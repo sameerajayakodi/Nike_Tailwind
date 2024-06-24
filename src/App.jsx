22d764dad1218fe9b9a54ae4a35dfc33c1f75e3d
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-24 15:02:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = ()=>(
+<main className="relative">
+  Nav
+<section className="xl:padding-1 wide:padding-r padding:b">
+Hero
+</section>
+<section className="padding">
+  PopularProducts
+</section>
+<section className="padding">
+  SuperQuality
+</section>
+<section className="py-10 padding-x">
+  Services
+</section>
+<section className="padding">
+  SpecialOffer
+</section>
+<section className="padding bg-pale-blue">
+  CustomerReviews
+</section>
+<section className="w-full py-16 padding-x sm:py-32">
+  Subscribe
+</section>
+<section className="pb-8 bg-black padding padding-x padding-t">
+  Footer
+</section>
+</main>
+)
+
+export default App;
