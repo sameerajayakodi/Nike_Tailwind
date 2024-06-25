68f559d363e4b3ded982368e53737d3fb93b66d5
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-06-25 14:56:03 +0530

    update services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,11 +1,14 @@
-
+import ServiceCard from "../Components/ServiceCard";
+import { services } from "../constants";
 
 const Services = () => {
   return (
-    <div>
-      Services
-    </div>
-  )
-}
+    <section className='flex flex-wrap justify-center max-container gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
